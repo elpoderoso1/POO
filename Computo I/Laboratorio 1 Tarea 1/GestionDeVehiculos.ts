5118cc2dc5df99db8ea3652d03fa0ea693f2fe50
Seth <96531908+elpoderoso1@users.noreply.github.com>
2025-02-20 10:48:36 -0600

    10

--- Computo I/Laboratorio 1 Tarea 1/GestionDeVehiculos.ts
+++ Computo I/Laboratorio 1 Tarea 1/GestionDeVehiculos.ts
@@ -1,37 +1,50 @@
-abstract class Vehiculo {
+// Para el segundo ejercicio es libre de crear el programa que usted quiera pero tiene que cumplir con las siguientes condiciones.
+// Debe contener herencia (puede ser cualquier tipo de herencia)
+// Debe contener polimorfismo (puede ser cualquier tipo de polimorfismo)
+// Debe contener al menos una clase abstracta y un método abstracto
+// El ejercicio que realice debe ser funcional y tener sentido. No debe usar clases al azar solo para cumplir con los requisitos
+
+
+abstract class Vehiculo { // Clase abstracta
     constructor(
         public marca: string,
         public modelo: string
     ) {}
 
-    abstract mover(): void;
+    abstract mover(): void; // Método abstracto
 }
 
+// Herencia
 class Auto extends Vehiculo {
     constructor(marca: string, modelo: string, public tipo: string) {
         super (marca, modelo);
     }
 
+    // Polimorfismo
     public mover() {
         console.log(`${this.marca}:${this.modelo} (Auto) está conduciendo por la ciudad.`);
     }
 }
 
+// Herencia
 class Camion extends Vehiculo {
     constructor(marca: string, modelo: string, public capacidad: number) {
         super (marca, modelo);
     }
 
+    // Polimorfismo
     public mover() {
         console.log(`${this.marca}:${this.modelo} (Camión) está conduciendo por la ciudad.`);
     }
 }
 
+// Herencia
 class Moto extends Vehiculo {
     constructor(marca: string, modelo: string, public tipo: string) {
         super (marca, modelo);
     }
 
+    // Polimorfismo
     public mover() {
         console.log(`${this.marca}:${this.modelo} (Moto) está conduciendo por la ciudad.`);
     }
