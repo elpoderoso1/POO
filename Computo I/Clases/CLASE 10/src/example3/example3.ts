67ff7694d5e9565523d56136a53d2e834bf39ddc
Seth <96531908+elpoderoso1@users.noreply.github.com>
2025-02-22 11:55:53 -0600

    10

--- Computo I/Clases/CLASE 10/src/example3/example3.ts
+++ Computo I/Clases/CLASE 10/src/example3/example3.ts
@@ -5,20 +5,38 @@ function identifyExample<T>(arg: T) {
 console.log(identifyExample<string>("Hola"));
 console.log(identifyExample<number>(100));
 
-interface CRUDInterfaceExample<T>{
+interface CRUDInterfaceExample<T> {
     getList(): void;
     add(item: T): void;
 }
 
 class CRUDRepositoyExample<T> implements CRUDInterfaceExample<T> {
-    private items: T[] = [];
-    
+    private items: T [] = [];
+
     public getList(): void {
-        
+
     }
 
     public add(item: T): void {
         this.items.push(item);
     }
 
+}
+
+class ProductExample {
+    constructor(
+        public id: number,
+        public name: string,
+        public price: number,
+        public quantity: number
+    ) {}
+}
+
+class EmployeeExample {
+    constructor(
+        public id: number,
+        public name: string,
+        public email: string,
+        public salary: number
+    ) {}
 }
