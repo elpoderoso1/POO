5ca97efbc2757fca545b4ab4377975b1f39de148
Seth <96531908+elpoderoso1@users.noreply.github.com>
2025-02-22 12:09:56 -0600

    10

--- Computo I/Clases/CLASE 10/src/example3/example3.ts
+++ Computo I/Clases/CLASE 10/src/example3/example3.ts
@@ -1,7 +1,6 @@
 function identifyExample<T>(arg: T) {
     return arg;
 }
-
 console.log(identifyExample<string>("Hola"));
 console.log(identifyExample<number>(100));
 
@@ -14,7 +13,7 @@ class CRUDRepositoyExample<T> implements CRUDInterfaceExample<T> {
     private items: T [] = [];
 
     public getList(): void {
-
+        
     }
 
     public add(item: T): void {
@@ -44,3 +43,14 @@ class EmployeeExample {
 const newProductExample = new CRUDRepositoyExample<ProductExample>();
 const newEmployeeExample = new CRUDRepositoyExample<EmployeeExample>();
 
+newProductExample.add({ id: 1, name: 'Producto', price: 20, quantity: 100 });
+newProductExample.add({ id: 2, name: 'Producto 2', price: 30, quantity: 200 });
+newProductExample.add({ id: 3, name: 'Producto 3', price: 40, quantity: 300 });
+newProductExample.add({ id: 4, name: 'Producto 4', price: 50, quantity: 400 });
+newProductExample.getList();
+
+newEmployeeExample.add({ id: 1, name: 'Juan Perez', email: 'juanperez@gmail.com', salary: 1000 });
+newEmployeeExample.add({ id: 2, name: 'Ana Abigail', email: 'anaabigail@gmail.com', salary: 1000 });
+newEmployeeExample.add({ id: 3, name: 'Jose Jose', email: 'josejose@gmail.com', salary: 1000 });
+newEmployeeExample.add({ id: 4, name: 'Esther Luna', email: 'estherll@gmail.com', salary: 1000 });
+newEmployeeExample.getList();
